ee9b8a88efd2865bf5e9a7639fe37a0800b92cc6
Marcos Antônio Barbosa de Souza <desouza.marcos@uol.com.br>
2018-02-20 21:03:58 -0300

    Primeiro e segundo desafios concluidos

---
+++ HackerHank01/HackerHank01/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerHank01 {
+    internal class Program {
+        private static int simpleArraySum(int n, int[] ar) {
+            int resultado = 0;
+            foreach (int x in ar) {
+                resultado += x;
+                Console.WriteLine("Resultado=" + x);
+            }
+            return resultado;
+        } 
+
+        private static void Main(String[] args) {
+            int n = Convert.ToInt32(Console.ReadLine());
+            string[] ar_temp = Console.ReadLine().Split(' ');
+            int[] ar = Array.ConvertAll(ar_temp, Int32.Parse);
+            int result = simpleArraySum(n, ar);
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+}
+
---
+++ HackerHank02/HackerHank02/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace HackerHank02 {
+    class Program {
+        static int[] solve(int a0, int a1, int a2, int b0, int b1, int b2) {
+            int a = 0, b = 0;
+            int[] resultado = new int[2];
+            if (a0 > b0) {
+                a += 1;
+            } else if (b0 > a0) {
+                b += 1;
+            }
+            if (a1 > b1) {
+                a += 1;
+            } else if (b1 > a1) {
+                b += 1;
+            }
+            if (a2 > b2) {
+                a += 1;
+            } else if (b2 > a2) {
+                b += 1;
+            }
+
+            resultado[0] = a;
+            resultado[1] = b;
+
+            return resultado;
+        }
+
+        static void Main(String[] args) {
+            string[] tokens_a0 = Console.ReadLine().Split(' ');
+            int a0 = Convert.ToInt32(tokens_a0[0]);
+            int a1 = Convert.ToInt32(tokens_a0[1]);
+            int a2 = Convert.ToInt32(tokens_a0[2]);
+            string[] tokens_b0 = Console.ReadLine().Split(' ');
+            int b0 = Convert.ToInt32(tokens_b0[0]);
+            int b1 = Convert.ToInt32(tokens_b0[1]);
+            int b2 = Convert.ToInt32(tokens_b0[2]);
+            int[] result = solve(a0, a1, a2, b0, b1, b2);
+            Console.WriteLine(String.Join(" ", result));
+
+            Console.ReadLine();
+        }
+    }
+}
